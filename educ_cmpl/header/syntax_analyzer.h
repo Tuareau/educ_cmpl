55ff20aecc9b63857ee3e3bfac1f5f2a62ea7550
Павел Черемшагин <pavel280101@outlook.com>
2021-11-26 23:39:13 +0300

    try solve problem

--- educ_cmpl/header/syntax_analyzer.h
+++ educ_cmpl/header/syntax_analyzer.h
@@ -4,9 +4,42 @@
 class syntax_analyzer
 {
 private:
-
+	LexicalAnalyzer* LexA;
+	/*typedef enum class bool {
+		OK, TYPE, OPERATOR, OPERATION, KEYWORDS, DELIMITERS, OPER_SIGN
+	}ret;*/
+	bool S();
+	bool PROGRAM_BODY();
+	bool LANG_CONSTRUCT();
+	bool VAR_DECL();
+	bool TYPE();
+	bool IDENT();
+	bool FUNC_DEF();
+	bool FUNC_DECL();
+	bool FUNC_PARAM();
+	bool FUNC_BODY();
+	bool FUNC_CONSTRUCT();
+	bool RETURN_OP();
+	bool RETURN_EXPR();
+	bool FUNC_CALL();
+	bool ARG();
+	bool ARYTHM_EXPR();
+	bool ARYTHM_OPERATION();
+	bool OP();
+	bool OPERAND();
+	bool CONST_EXPR();
+	bool LOGICAL_EXPR();
+	bool LOGICAL_OPERATION();
+	bool LOGICAL_OP();
+	bool LOGICAL_STATEMENT();
+	bool LANG_OPERATOR();
+	bool WRITE_OP();
+	bool WHILE_OP();
+	bool IF_OP();
+	bool OPERATION();
 public:
-	bool left_op();
-	bool middle_op();
-	bool right_op();
+	syntax_analyzer(LexicalAnalyzer* la);
+	void synt_analyz();
+	Token get_token();
+	~syntax_analyzer();
 };
--- educ_cmpl/source/syntax_analyzer.cpp
+++ educ_cmpl/source/syntax_analyzer.cpp
@@ -1,2 +1,222 @@
 #include "syntax_analyzer.h"
 
+using std::cout;
+using std::cin;
+using std::endl;
+//typedef enum class RET_ERR {
+//	OK, TYPE, OPERATOR, OPERATION, KEYWORDS, DELIMITERS, OPER_SIGN
+//}ret;
+
+syntax_analyzer::~syntax_analyzer()
+{
+
+}
+
+syntax_analyzer::syntax_analyzer(LexicalAnalyzer* la) { LexA = la; }
+
+void syntax_analyzer::synt_analyz()
+{
+	int i = 0;
+	
+}
+
+Token syntax_analyzer::get_token() { return (LexA->get_next_token()); }
+
+bool syntax_analyzer::S()
+{
+	auto token = get_token();
+	if (token.value() != "PROGRAMM") {
+		cout << "ERROR! Expected 'PROGRAMM'." << endl;
+		return false;
+	}
+	token = get_token();
+	if (token.value() != ";") {
+		cout << "ERROR! Expected ';'." << endl;
+		return false;
+	}
+	PROGRAM_BODY();
+	token = get_token();
+	if (token.value() != "END") {
+		cout << "ERROR! Expected 'END'." << endl;
+		return false;
+	}
+	token = get_token();
+	if (token.value() != ".") {
+		cout << "ERROR! Expected '.'." << endl;
+		return false;
+	}
+	return true;
+}
+
+bool syntax_analyzer::PROGRAM_BODY()
+{
+	if (!LANG_CONSTRUCT())
+		return false;
+	return true;
+}
+
+bool syntax_analyzer::LANG_CONSTRUCT()
+{
+	auto token = get_token();
+	VAR_DECL();
+	FUNC_DEF();
+	ARYTHM_EXPR(); get_token() != “; ”;
+	FUNC_CALL(); get_token() != “; ”;
+	LANG_OPERATOR();
+}
+
+bool syntax_analyzer::VAR_DECL()
+{
+	auto token = get_token();
+	TYPE();
+	IDENT();
+	if (token.value() != ";")
+	{
+		cout << "Expected ';'" << endl;
+		return false;
+	}
+	return true;
+}
+
+bool syntax_analyzer::TYPE()
+{
+	auto token = get_token();
+	if (token.value() != "integer" || token.value() != "string" || token.value() != "bool")
+	{
+		cout << "Expected type." << endl;
+		return false;
+	}
+	return true;
+}
+
+bool syntax_analyzer::IDENT()
+{
+	return false;
+}
+
+bool syntax_analyzer::FUNC_DEF()
+{
+	return false;
+}
+
+bool syntax_analyzer::FUNC_DECL()
+{
+	return false;
+}
+
+bool syntax_analyzer::FUNC_PARAM()
+{
+	return false;
+}
+
+bool syntax_analyzer::FUNC_BODY()
+{
+	return false;
+}
+
+bool syntax_analyzer::FUNC_CONSTRUCT()
+{
+	return false;
+}
+
+bool syntax_analyzer::RETURN_OP()
+{
+	return false;
+}
+
+bool syntax_analyzer::RETURN_EXPR()
+{
+	return false;
+}
+
+bool syntax_analyzer::FUNC_CALL()
+{
+	return false;
+}
+
+bool syntax_analyzer::ARG()
+{
+	return false;
+}
+
+bool syntax_analyzer::ARYTHM_EXPR()
+{
+	return false;
+}
+
+bool syntax_analyzer::ARYTHM_OPERATION()
+{
+	return false;
+}
+
+bool syntax_analyzer::OP()
+{
+	return false;
+}
+
+bool syntax_analyzer::OPERAND()
+{
+	return false;
+}
+
+bool syntax_analyzer::CONST_EXPR()
+{
+	auto token = get_token();
+	if (token.)
+	{
+		cout << "" << endl;
+		return false;
+	}
+	return true;
+}
+
+bool syntax_analyzer::LOGICAL_EXPR()
+{
+	return false;
+}
+
+bool syntax_analyzer::LOGICAL_OPERATION()
+{
+	return false;
+}
+
+bool syntax_analyzer::LOGICAL_OP()
+{
+	auto token = get_token();
+	if (token.value() == ">" || token.value() == "<" || token.value() == "==")
+	{
+		cout << "Expected logical operator." << endl;
+		return false;
+	}
+	return true;
+}
+
+bool syntax_analyzer::LOGICAL_STATEMENT()
+{
+	return false;
+}
+
+bool syntax_analyzer::LANG_OPERATOR()
+{
+	return false;
+}
+
+bool syntax_analyzer::WRITE_OP()
+{
+	return false;
+}
+
+bool syntax_analyzer::WHILE_OP()
+{
+	return false;
+}
+
+bool syntax_analyzer::IF_OP()
+{
+	return false;
+}
+
+bool syntax_analyzer::OPERATION()
+{
+	return false;
+}
