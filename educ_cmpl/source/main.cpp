115bd42744fcea5dfb45679263ef12df923cca9c
Pavel Matviyuk <pav.matt@mail.ru>
2021-11-29 13:43:06 +0300

    Диалоговая оболочка в main()
    Сообщения о процессе интерпретации,
    ошибках открытия файла, синтаксических
    ошибках.

--- educ_cmpl/source/main.cpp
+++ educ_cmpl/source/main.cpp
@@ -8,33 +8,51 @@
 
 int main(int argc, char * argv[]) {
 	if (argc != 2) {
-		std::cout << "USAGE: interpreter filename";
+		std::cout << "\n>>> usage: interpreter filename\n";
 		return 1;
 	}
 
 	std::string filename = argv[1];
+	std::fstream fhandler;
+	fhandler.open(filename);
+	if (!fhandler.good()) {
+		std::cout << "\n>>> file not found";
+		std::cout << "\n>>> usage: interpreter filename\n";
+		return 1;
+	}
 
-	std::ofstream fout("tokens.txt", std::ios_base::trunc);
+	std::ofstream fout_tokens("tokens.txt", std::ios_base::trunc);
+	std::ofstream fout_idents("idents.txt", std::ios_base::trunc);
+	std::ofstream fout_constants("constants.txt", std::ios_base::trunc);
+	std::ofstream fout_rpn("rpn.txt", std::ios_base::trunc);
+	std::ofstream fout_init_memory("memory.txt", std::ios_base::trunc);
+
+	std::cout << "\n>>> build started...\n\n";
 
 	LexicalAnalyzer la(filename);
 	la.construct_token_table();
-	la.print_token_table(fout);
+	la.print_token_table(fout_tokens);
 
 	SyntaxAnalyzer sa(&la);
 	auto is_correct = sa.analyze_syntax();
 
 	if (is_correct) {
 		la.construct_ident_table();
-		la.print_ident_table(std::cout);
+		la.print_ident_table(fout_idents);
 		la.construct_constant_table();
-		la.print_constant_table(std::cout);
+		la.print_constant_table(fout_constants);
 
 		Interpreter ipr(&la);
 		ipr.construct_notation_output();
-		ipr.print_notation(std::cout);
+		ipr.print_notation(fout_rpn);
 		ipr.initialize_memory();
-		ipr.print_memory(std::cout);
+		ipr.print_memory(fout_init_memory);
 		ipr.execute_notation();
+
+		std::cout << "\n\n>>> successfuly executed\n";
+	}
+	else {
+		std::cout << "\n>>> could not start interpretation due to errors\n";
 	}
 
 	return 0;
